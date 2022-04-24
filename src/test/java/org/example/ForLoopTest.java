55d81a783b6d4be80078945bfc86845e0ef7c246
Hieu Tran <hieu.t.tran4@boeing.com>
2022-04-24 10:17:00 +1000

    Begin chapter 10

---
+++ src/test/java/org/example/ForLoopTest.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class ForLoopTest {
+    @Test
+    public void testCase1() throws IOException {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("forLoop.lox");
+        String source = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+        Scanner scanner = new Scanner(source);
+
+        List<Token> tokens = scanner.scanTokens();
+        Parser parser = new Parser(tokens);
+        List<Stmt> stmts = parser.parse();
+
+        Interpreter interpreter = new Interpreter();
+        interpreter.interpret(stmts);
+
+        Environment global = interpreter.getEnvironment();
+        Object x = global.getValues().get("a");
+        Assert.assertTrue(x instanceof Double);
+        Assert.assertEquals(((Double) x).doubleValue(), 10, 0.0001);
+    }
+}
